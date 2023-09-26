f3cd1d47438b3b20f5ea4bbe3c6a320dc19c3472
Diego Medina <diegocab456@gmail.com>
2023-09-25 23:47:41 -0300

    marcadores dinamicos

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
@@ -6,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class MarkersPageComponent {
 
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-64.11026450502766, -31.354763290465172);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap)
+      throw "El elemento html no fue encontrado";
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    /* FORMA DE CREAR UN MARKER EN MAPBOXS */
+    //const markerHtml = document.createElement('div');
+    //markerHtml.innerHTML = 'Diego';
+
+    //const marker = new Marker({
+    //  //color: 'red'
+    //  element: markerHtml
+    //})
+    //.setLngLat( this.currentLngLat ).addTo( this.map );
+  }
+
+  public createMarker() {
+    if (!this.map)
+      return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map!.getCenter();
+    this.addMarker( lngLat, color );
+  }
+
+  public addMarker( lngLat: LngLat, color: string ) {
+    if (!this.map)
+      return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+    .setLngLat( lngLat ).addTo( this.map );
+  }
 }
