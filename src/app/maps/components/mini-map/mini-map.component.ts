57004729ff28fd6f25b3fe934f7e0ae2965e7da3
Diego Medina <diegocab456@gmail.com>
2023-09-28 00:07:10 -0300

    listado de propiedades - minimap

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
 
+  @Input()
+  public lngLat?: [number, number];
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap?.nativeElement)
+      throw "Map div not found";
+
+    if (!this.lngLat)
+      throw "LngLat cant be null";
+    //mapa
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+
+    //marker inicial
+    new Marker({
+      color: "red",
+    })
+    .setLngLat( this.lngLat ).addTo( this.map );
+  }
 }
