d00039e63f5eadd5abf8b05b57905122bc331f93
Diego Medina <diegocab456@gmail.com>
2023-09-28 22:22:42 -0300

    creacion alonPageComponent standalone

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -17,7 +17,7 @@ export class FullScreenPageComponent implements AfterViewInit {
     const map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: [-64.18105, -31.4135], // starting position [lng, lat]
       zoom: 9, // starting zoom
       });
   }
